3b031fd91bdc223aa4ba6c0b2484e7281e6ab4e3
olgarazmolodina <razmolodinaolga01@gmail.com>
2024-11-20 12:59:20 +0200

    new

--- src/test/java/tests/SelectorsCss.java
+++ src/test/java/tests/SelectorsCss.java
@@ -1,4 +1,47 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class SelectorsCss {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+public void elementsHeaderIlCarro(){
+        driver.navigate().to("https://ilcarro.web.app/search");
+
+
+        WebElement btnSearchHeader= driver.findElement(By.cssSelector("a[href='search']"));
+        System.out.println("tag name --> " + btnSearchHeader.getTagName());
+        System.out.println("text --> " + btnSearchHeader.getText());
+
+        WebElement btnLetsCarHeader = driver.findElement(By.cssSelector("*[href='let-car-work']"));
+        System.out.println("tag name --> " + btnSearchHeader.getTagName());
+        System.out.println("text --> " + btnSearchHeader.getText());
+
+        WebElement btnTermsHeader = driver.findElement(By.cssSelector("*[href='terms-of-use']"));
+        System.out.println("tag name --> " + btnTermsHeader.getTagName());
+        System.out.println("text --> " + btnTermsHeader.getText());
+
+        WebElement btnSindupHeader = driver.findElement(By.cssSelector("a[href='registration']"));
+        System.out.println("tag name --> " + btnSindupHeader.getTagName());
+        System.out.println("text --> " + btnSindupHeader.getText());
+
+        WebElement btnLoginHeader = driver.findElement(By.cssSelector("a[href='login']"));
+        System.out.println("tag name --> " + btnLoginHeader.getTagName());
+        System.out.println("text --> " + btnLoginHeader.getText());
+
+
+
+
+
+         //driver.quit();
+
+
+
+
+    }
 }
--- src/test/java/tests/StartTest.java
+++ src/test/java/tests/StartTest.java
@@ -1,4 +1,53 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
 public class StartTest {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void firstTest() {
+
+        driver.navigate().to("https://telranedu.web.app/home");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement btnLoginHeader= driver.findElement(By.cssSelector("a[href='/login']"));
+        btnLoginHeader.click();
+
+        WebElement btnRegistration = driver.findElement(By.cssSelector("button[name='registration']"));
+        btnRegistration.click();
+
+
+        //driver.quit();
+
+    }
+
+
+ @Test
+    public void secondTest()
+ {
+     driver.navigate().to("https://telranedu.web.app/home");
+     WebElement elementText = driver.findElement(By.cssSelector("h2"));
+     System.out.println(elementText.getText());
+     try {
+         Thread.sleep(3000);
+     } catch (InterruptedException e) {
+         throw new RuntimeException(e);
+     }
+     WebElement btmAbout = driver.findElement(By.cssSelector("*[href='/about']"));
+     System.out.println("tag name --> " + btmAbout.getTagName());
+     System.out.println("text --> " + btmAbout.getText());
+
+     driver.quit();
+
+ }
 }
